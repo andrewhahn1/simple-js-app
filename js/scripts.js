a90c2a0bf859a9c1b085d5db57b87b9732d642bf
andrewhahn1 <153559295+andrewhahn1@users.noreply.github.com>
2024-01-04 16:41:44 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
+    {name: 'Charmander', height: 2, types: 'fire'},
+    {name: 'Squirtle', height: 1, types: 'water'}
+]
