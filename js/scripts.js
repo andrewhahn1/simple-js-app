52ca99cadd34ce60ab9c332c0fe698c943eb157a
andrewhahn1 <153559295+andrewhahn1@users.noreply.github.com>
2024-01-09 14:15:33 -0500

    Update scripts.js
    If committed, added a loop with conditions for each item in the pokemon array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,28 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 2, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 2, types: 'fire'},
-    {name: 'Squirtle', height: 1, types: 'water'}
+    {
+    name: 'Bulbasaur', 
+    height: 2.04, 
+    types: ['grass', 'poison']
+    },
+    {
+    name: 'Charmander', 
+    height: 2, 
+    types: ['fire']
+    },
+    {
+    name: 'Squirtle', 
+    height: 1.08, 
+    types: ['water']
+    }
 ]
+
+for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].height < 5 && pokemonList[i].height > 2){
+        document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + ' – What a good little guy!');
+    }else if (pokemonList[i].height === 2){
+        document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height);
+    }else {
+        document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height)
+    }
+
+}
