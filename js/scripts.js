9b39168ac053ccc248aba94a8165f016e106c0f7
andrewhahn1 <153559295+andrewhahn1@users.noreply.github.com>
2024-01-15 20:19:11 -0500

    Update scripts.js
    If committed, want to loop over the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,41 @@
-let pokemonList = [
-    {
-    name: 'Bulbasaur', 
-    height: 2.04, 
-    types: ['grass', 'poison']
-    },
-    {
-    name: 'Charmander', 
-    height: 2, 
-    types: ['fire']
-    },
-    {
-    name: 'Squirtle', 
-    height: 1.08, 
-    types: ['water']
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 2.04,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 2,
+            types: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 1.08,
+            types: ['water']
+        }
+    ]
+    
+    function getAll () {
+        return pokemonList;
     }
-]
+    
+    function add (pokemon){
+       pokemonList.push (pokemon); 
+    }
+   
+    return {
+        getAll: getAll,
+        add: add,
+    }
 
-pokemonList.forEach(function(pokemon) {
+})();
+
+document.write(pokemonRepository.getAll());
+
+function myLoop(pokemon) {
     document.write(pokemon.name + ': ' + pokemon.height + '<br>' + 'type: ' + pokemon.types + '<br><br>');
-  });
-
+};
+pokemonRepository.forEach(myLoop);
